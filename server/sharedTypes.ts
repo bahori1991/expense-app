d00e01faca45a40260f2f0103b9348c88e49533e
bahori <bahori2011ms@gmail.com>
2024-12-10 22:29:39 +0900

    set optimistic UI server

--- server/sharedTypes.ts
+++ server/sharedTypes.ts
@@ -1,7 +1,10 @@
-import { z } from "zod";
 import { insertExpenseSchema } from "./db/schema/expenses";
+import { z } from "zod";
 
 export const createExpenseSchema = insertExpenseSchema.omit({
   userId: true,
   createdAt: true,
+  id: true,
 });
+
+export type CreateExpense = z.infer<typeof createExpenseSchema>;
